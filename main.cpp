cf123d1bf423a4410711ae2f165640d3647fa2ab
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-10-27 15:05:42 +0000

    Eccezioni Multiple

--- main.cpp
+++ main.cpp
@@ -10,6 +10,37 @@ int compare(int a, int b){
     return a == b;
 }
 
+int dividi(int a, int b){
+    if(a < 0 || b < 0) {
+        throw invalid_argument("❌ Valori negativi!");
+    }
+    if(b == 0){
+        throw overflow_error("❌ Divisione per zero!");
+    }
+    if(a < b){
+        throw out_of_range("❌ a < b!");
+    }
+    return a/b;
+}
+
+void test2(){
+    try{
+        cout << dividi(3, 12) << endl;
+    } catch(const invalid_argument& e){
+        cout << "❌ Valori negativi!" << endl;
+    } catch(const overflow_error& e){
+        cout << "❌ Divisione per zero!" << endl;
+    } catch(const out_of_range& e){
+        cout << "❌ a < b!" << endl;
+    } catch(...){
+        cout << "❌ ERRORE GENERICO" << endl;
+    }
+
+    /*
+        catch(...) gestisce tutte le eccezioni
+    */
+}
+
 void test(){
     try{ 
         cout << compare(2, -4) << endl;
@@ -37,7 +68,9 @@ int main() {
     // cout << "continua..." << endl;
     
     
-    test();
+    //test();
+
+    test2();
 
     return 0;
 } 
