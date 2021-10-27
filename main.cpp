d5c92470891eb56505f641cc65114554d0191ba2
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-10-27 14:33:02 +0000

    Primo sguardo alle eccezioni

--- main.cpp
+++ main.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 int main() {
-    
+    double x, y;
+    //la prima eccezione valida interrompe il codice nel try
+    try{
+        cout << "Dividendo: "; cin >> x;
+        cout << "Divisore: ", cin >> y;
+        if(x > 10){
+            throw "Dividendo troppo grande!";
+        }
+        if(y == 0) {
+            throw "Divisione per zero!";
+        }
+        cout << x/y << endl;
+    } catch(const char* err) {
+        cout << err << endl;  
+    }
+    cout << "continua..." << endl;
     return 0;
 } 
